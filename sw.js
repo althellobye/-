a8b4168fa49666432fce6a9552b79101af0d5d03
althellobye <johnnylao1228@gmail.com>
2026-08-05 12:31:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,41 +2,29 @@ const CACHE_NAME = 'cherry-space-v1';
 const urlsToCache = [
   '/-/',
   '/-/index.html',
-  '/-/manifest.json',
-  '/-/data.json'
+  '/-/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('快取已建立');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
 
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
         })
       );
     })
