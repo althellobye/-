c1e79a3c20bafb7d6ab44a99171d12a097e044bb
althellobye <johnnylao1228@gmail.com>
2026-08-05 12:20:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@ const CACHE_NAME = 'cherry-space-v1';
 const urlsToCache = [
   '/-/',
   '/-/index.html',
-  '/-/manifest.json'
+  '/-/manifest.json',
+  '/-/data.json'
 ];
 
 self.addEventListener('install', event => {
